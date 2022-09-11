f459fefc568aaba3f1068a8116dee6c3ba4cef1b
Sakil Anwar <sakilanwar60@gmail.com>
2022-09-11 08:51:41 +0600

    fix mobile menu issue

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     // mobile menu
     const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
-    const mobileMenu = document.querySelector('.mobile-menu ');
+    const mobileMenu = document.querySelector('.spellon-mobile-menu ');
     const body = document.querySelector('body');
     mobileMenuBtn.addEventListener('click', function () {
         mobileMenu.classList.toggle('show-mobile-menu')
